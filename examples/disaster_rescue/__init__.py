fa05b56c2583ff9facf7b27c935fe40f80031799
shutakahama <shutaka.sukehama@gmail.com>
2025-12-07 17:09:45 +0900

    create disaster rescue experiment

---
+++ examples/disaster_rescue/__init__.py
@@ -0,0 +1,8 @@
+"""
+Disaster Rescue Task for ShinkaEvolve
+
+This package implements a disaster rescue coordination task where multiple
+rescue vehicles must work together to save victims with varying severity levels.
+"""
+
+__version__ = "1.0.0"
